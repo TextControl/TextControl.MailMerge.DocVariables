f58894273b84ca3561ca4027012991ffdf3f092b
bjoerntx <bjoern@textcontrol.com>
2015-06-09 18:41:17 +0200

    Uploaded sample

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApplication31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textControl1 = new TXTextControl.TextControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mailMergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mailMerge1 = new TXTextControl.DocumentServer.MailMerge(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(0, 24);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.Size = new System.Drawing.Size(753, 461);
+            this.textControl1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mailMergeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(753, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mailMergeToolStripMenuItem
+            // 
+            this.mailMergeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeToolStripMenuItem});
+            this.mailMergeToolStripMenuItem.Name = "mailMergeToolStripMenuItem";
+            this.mailMergeToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.mailMergeToolStripMenuItem.Text = "Mail Merge";
+            // 
+            // mergeToolStripMenuItem
+            // 
+            this.mergeToolStripMenuItem.Name = "mergeToolStripMenuItem";
+            this.mergeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mergeToolStripMenuItem.Text = "Merge";
+            this.mergeToolStripMenuItem.Click += new System.EventHandler(this.mergeToolStripMenuItem_Click);
+            // 
+            // mailMerge1
+            // 
+            this.mailMerge1.TextComponent = this.textControl1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 485);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TX Text Control MailMerge: Merge DocVariables";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mailMergeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeToolStripMenuItem;
+        private TXTextControl.DocumentServer.MailMerge mailMerge1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl;
+using TXTextControl.DocumentServer.Fields;
+
+namespace WindowsFormsApplication31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mergeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TXTextControl.LoadSettings ls = new TXTextControl.LoadSettings();
+            ls.ApplicationFieldFormat = TXTextControl.ApplicationFieldFormat.MSWord;
+
+            textControl1.Load("template.docx", TXTextControl.StreamType.WordprocessingML, ls);
+
+            // loop through all fields in all text parts
+            foreach (IFormattedText textPart in textControl1.TextParts)
+            {
+                foreach (ApplicationField appField in textPart.ApplicationFields)
+                {
+                    // convert all "DocVariable" fields to "MergeFields"
+                    if (appField.TypeName == "DOCVARIABLE")
+                    {
+                        MergeField newField = new MergeField();
+                        newField.Name = appField.Parameters[0];
+
+                        appField.Parameters = newField.ApplicationField.Parameters;
+                        appField.TypeName = "MERGEFIELD";
+                        appField.Text = newField.Name;
+                    }
+                }
+            }
+
+            address newAddress = new address("Peter", "Text Control, LLC");
+            mailMerge1.MergeObjects(new List<address>() { newAddress });
+        }
+    }
+
+    public class address
+    {
+        public string name { get; set; }
+        public string company { get; set; }
+
+        public address(string name, string company)
+        {
+            this.name = name;
+            this.company = company;
+        }
+    }
+}
